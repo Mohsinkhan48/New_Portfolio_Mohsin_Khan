fa64d5a5497bfbe6961f0e98361777dbcfd100c5
Mohsinkhan48 <kmohsinkhan482@gmail.com>
2026-03-20 07:13:03 +0500

    map key added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   basePath: "/portfolio", // replace 'portfolio' with your repo name
+  trailingSlash: true, // helps with GitHub Pages routing
 };
 
 module.exports = nextConfig;
